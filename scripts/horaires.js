67758f36a74badf9507cdbd846b0c80f132bbe4f
batsWTR <baptiste.wentzler@wanadoo.fr>
2021-11-09 13:05:23 +0100

    css page liens

--- scripts/horaires.js
+++ scripts/horaires.js
@@ -1,21 +1,22 @@
 
 
 // ce script affiche en vert le jour si ouvert ou rouge si fermé
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    let today = new Date();
-    let jour = today.getDay();
-
-    if(jour === 1 || jour === 4 || jour === 5 || jour === 6){
-        ouvert = document.getElementById(jour).style.color = 'green';
-        heures = document.getElementById(jour).nextElementSibling;
-        heures.style.color = 'green';
-    }
-    else{
-        ouvert = document.getElementById(jour).style.color = 'red';
-        heures = document.getElementById(jour).nextElementSibling;
-        heures.style.color = 'red';
-    }
-
-
-});
+(function(){
+    document.addEventListener('DOMContentLoaded', ()=>{
+        let today = new Date();
+        let jour = today.getDay();
+    
+        if(jour === 1 || jour === 4 || jour === 5 || jour === 6){
+            ouvert = document.getElementById(jour).style.color = 'green';
+            heures = document.getElementById(jour).nextElementSibling;
+            heures.style.color = 'green';
+        }
+        else{
+            ouvert = document.getElementById(jour).style.color = 'red';
+            heures = document.getElementById(jour).nextElementSibling;
+            heures.style.color = 'red';
+        }
+    
+    
+    });
+})()
